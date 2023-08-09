8a77f91d236993452c8b2f55be89416aaf2fa786
M-Hassan1 <hassan81932@gmail.com>
2023-08-09 09:32:25 -0700

    Added Animation in css

--- src/Components/HeroSection/Home.jsx
+++ src/Components/HeroSection/Home.jsx
@@ -1,16 +1,33 @@
-import React from 'react'
-import photo from '../assets/Images/calvin-dahne-WX9bvBTUzB4-unsplash.jpg'
+// import React from 'react'
+// import photo from '../assets/Images/calvin-dahne-WX9bvBTUzB4-unsplash.jpg'
 
+
+// const Home = () => {
+//   return (
+//     <div  className='relative w-full h-screen'>
+//       <img src={photo} alt="Car" className='w-full h-full object-cover absolute ' />
+//       <h1 className='absolute bottom-5 left-10 p-3 text-pink-600 text-xl'>
+//         Best Car Rental Website for Big Tours
+//       </h1>
+//     </div>
+//   )
+// }
+
+// export default Home
+
+
+import React from 'react';
+import photo from '../assets/Images/calvin-dahne-WX9bvBTUzB4-unsplash.jpg';
 
 const Home = () => {
   return (
-    <div  className='relative w-full h-screen'>
-      <img src={photo} alt="Car" className='w-full h-full object-cover absolute ' />
+    <div className='relative w-full h-screen'>
+      <img src={photo} alt="Car" className='w-full h-full object-cover absolute animate-fade-in' />
       <h1 className='absolute bottom-5 left-10 p-3 text-pink-600 text-xl'>
         Best Car Rental Website for Big Tours
       </h1>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
